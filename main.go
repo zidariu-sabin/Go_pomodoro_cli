2ab8fe83464d43ae28d37cb0174a3dbb36e9539e
zidariu-sabin <zidariu19@gmail.com>
2025-08-15 16:10:06 +0300

    added rounds functionality and removed unnecessary packages

--- main.go
+++ main.go
@@ -12,63 +12,64 @@ import (
 )
 
 func main() {
-	// cmd.Execute()
-	// for {
-	// 	fmt.Printf("Time Remaining: %.1f", internal.TimeRemaining.Seconds())
-	// }
 	var workTime int
 	var breakTime int
+	var rounds int
 	flag.IntVar(&workTime, "w", 5, "duration of the working time")
 	flag.IntVar(&breakTime, "b", 5, "duration of the break time")
+	flag.IntVar(&rounds, "r", 4, "number of rounds the user wants the timer to run for")
 	flag.Parse()
 
 	secondsRemaining := workTime * 60
 	minutes := secondsRemaining / 60
 	seconds := secondsRemaining % 60
-	//duration given is the duration after run that the timer will start
-	timer := time.NewTimer(time.Second)
 	//duration given is the duration between the ticks
 	ticker := time.NewTicker(time.Second)
 
-	defer ticker.Stop()
 	isWorkPeriod := true
 	workColor := color.New(color.FgCyan).SprintFunc()
 	breakColor := color.New(color.FgHiGreen).SprintFunc()
-
+	done := make(chan struct{})
 	go func() {
-		for range ticker.C {
-			if secondsRemaining > 0 {
+		roundsRemaining := rounds
+		for roundsRemaining >= 0 {
+			if isWorkPeriod {
+				secondsRemaining = workTime * 60
+			} else {
+				secondsRemaining = breakTime * 60
+			}
+			for secondsRemaining > 0 {
 				minutes = secondsRemaining / 60
 				seconds = secondsRemaining % 60
 				if isWorkPeriod {
-					fmt.Printf("\r %s timer for %v minutes has started!  Time Remaining: %s   ",
+					fmt.Printf("\r Round %v/%v %s timer for %v minutes has started!  Time Remaining: %s   ",
+						4-roundsRemaining,
+						rounds,
 						workColor("Work"),
 						workTime,
 						workColor(fmt.Sprintf("%02d:%02d", minutes, seconds)),
 					)
 				} else {
-					fmt.Printf("\r %s timer for %v minutes has started!  Time Remaining: %s   ",
+					fmt.Printf("\r Round %v/%v %s timer for %v minutes has started!  Time Remaining: %s   ",
+						4-roundsRemaining,
+						rounds,
 						breakColor("Break"),
 						breakTime,
 						breakColor(fmt.Sprintf("%02d:%02d", minutes, seconds)),
 					)
 				}
+				<-ticker.C
 				secondsRemaining--
 			}
-			if secondsRemaining == 0 {
-				isWorkPeriod = !isWorkPeriod
-				if isWorkPeriod {
-					secondsRemaining = workTime * 60
-				} else {
-					secondsRemaining = breakTime * 60
-				}
+			isWorkPeriod = !isWorkPeriod
+			//a break period has ended, a round has elapsed
+			if isWorkPeriod {
+				roundsRemaining--
 			}
 		}
+		done <- struct{}{}
 	}()
 
-	//necessary to read the ticker, time.Sleep also takes time so ticker might only print 4 times
-	//#
-	time.Sleep(time.Duration(workTime+breakTime) * time.Minute)
-	timer.Stop()
-
+	<-done
+	ticker.Stop()
 }
