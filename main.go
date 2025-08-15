d20a538f7e7b483df08f6e4924e273426fe43dd3
zidariu-sabin <zidariu19@gmail.com>
2025-08-15 14:18:46 +0300

    removed extra unnecessary timer, added better console representation

--- main.go
+++ main.go
@@ -18,46 +18,48 @@ func main() {
 	// }
 	var workTime int
 	var breakTime int
-	flag.IntVar(&workTime, "workTime", 5, "duration of the working time")
-	flag.IntVar(&breakTime, "breakTime", 5, "duration of the break time")
+	flag.IntVar(&workTime, "w", 5, "duration of the working time")
+	flag.IntVar(&breakTime, "b", 5, "duration of the break time")
 	flag.Parse()
 
-	// secondsRemaining := workTime * 60
-	// minutes := secondsRemaining / 60
-	// seconds := secondsRemaining % 60
-	var secondsRemaining int
-	var minutes int
-	var seconds int
+	secondsRemaining := workTime * 60
+	minutes := secondsRemaining / 60
+	seconds := secondsRemaining % 60
 	//duration given is the duration after run that the timer will start
 	timer := time.NewTimer(time.Second)
 	//duration given is the duration between the ticks
 	ticker := time.NewTicker(time.Second)
 
 	defer ticker.Stop()
-	timerColor := color.New(color.FgCyan).SprintFunc()
+	isWorkPeriod := true
+	workColor := color.New(color.FgCyan).SprintFunc()
+	breakColor := color.New(color.FgHiGreen).SprintFunc()
 
 	go func() {
-		for {
-			//select keyword allows for initiating an individual await on each
-			//channel operation in the scope
-			select {
-			case <-timer.C:
-				fmt.Println("Work timer for", workTime, " minutes has started!")
-				//#
-				secondsRemaining = workTime * 60
-
+		for range ticker.C {
+			if secondsRemaining > 0 {
 				minutes = secondsRemaining / 60
 				seconds = secondsRemaining % 60
-			case <-ticker.C:
-				if secondsRemaining > 0 {
-					minutes = secondsRemaining / 60
-					seconds = secondsRemaining % 60
-					fmt.Printf("\rTime Remaining: %s   ", timerColor(fmt.Sprintf("%02d:%02d", minutes, seconds)))
-					secondsRemaining--
+				if isWorkPeriod {
+					fmt.Printf("\r %s timer for %v minutes has started!  Time Remaining: %s   ",
+						workColor("Work"),
+						workTime,
+						workColor(fmt.Sprintf("%02d:%02d", minutes, seconds)),
+					)
+				} else {
+					fmt.Printf("\r %s timer for %v minutes has started!  Time Remaining: %s   ",
+						breakColor("Break"),
+						breakTime,
+						breakColor(fmt.Sprintf("%02d:%02d", minutes, seconds)),
+					)
 				}
-				if secondsRemaining == 0 {
-					fmt.Println("\n Break timer for", breakTime, "minutes has started!")
-					//#
+				secondsRemaining--
+			}
+			if secondsRemaining == 0 {
+				isWorkPeriod = !isWorkPeriod
+				if isWorkPeriod {
+					secondsRemaining = workTime * 60
+				} else {
 					secondsRemaining = breakTime * 60
 				}
 			}
